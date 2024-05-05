bd3c07f184d0a12cd4f64ccd2938e5ceacca7023
klichardova <katarina.lichardova@gmail.com>
2024-05-05 17:56:33 +0200

    Change from EXP to CM9.04 board

--- source/KMR_dxluC_handler.cpp
+++ source/KMR_dxluC_handler.cpp
@@ -27,9 +27,9 @@
     #define DEBUG_SERIAL SerialUSB
     const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
 #elif defined(ARDUINO_OpenCM904) // When using official ROBOTIS board with DXL circuit.
-    #define DXL_SERIAL   Serial3 //OpenCM9.04 EXP Board's DXL port Serial. (Serial1 for the DXL port on the OpenCM 9.04 board)
+    #define DXL_SERIAL   Serial1 //OpenCM9.04 EXP Board's DXL port Serial. (Serial1 for the DXL port on the OpenCM 9.04 board)
     #define DEBUG_SERIAL Serial
-    const int DXL_DIR_PIN = 22; //OpenCM9.04 EXP Board's DIR PIN. (28 for the DXL port on the OpenCM 9.04 board)
+    const int DXL_DIR_PIN = 28; //OpenCM9.04 EXP Board's DIR PIN. (28 for the DXL port on the OpenCM 9.04 board)
     #define DEFAULT_BAUDRATE 57600
 #elif defined(ARDUINO_OpenCR) // When using official ROBOTIS board with DXL circuit.
     // For OpenCR, there is a DXL Power Enable pin, so you must initialize and control it.
